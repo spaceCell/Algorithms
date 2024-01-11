52fc81bae0626fcd85cf46b01ac0a27730a835dc
Kirill Kozlov <space_cell@proton.me>
2024-01-11 21:08:53 +0500

    Selection sort example

---
+++ src/SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+
+    // swap elements
+    public static void swapElements(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    // find low index
+    public static int indexLowest(int[] array, int start) {
+        int lowIndex = start;
+        for (int i = start; i < array.length; i++) {
+            if (array[i] < array[lowIndex]) {
+                lowIndex = i;
+            }
+        }
+        return lowIndex;
+    }
+
+    // sort elements
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int j = indexLowest(array, i);
+            swapElements(array, i, j);
+        }
+    }
+}
